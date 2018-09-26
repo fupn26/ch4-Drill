9c9c8f2995c6ecfb3b426c3b71da875fc2d1c1b4
fupn26 <43246998+fupn26@users.noreply.github.com>
2018-09-26 16:03:01 +0200

    Add files via upload

---
+++ ch4_drill1.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int first;
+int second;
+cout<<"PLease enter two integers!\n";
+while(cin>>first>>second)
+	{
+	cout<<first<<'\t'<<second<<'\n';
+	return 0;
+	}
+}
+
