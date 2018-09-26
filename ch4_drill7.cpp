afb140e5fb3cb48a7b2e8c7e7820d66006955878
fupn26 <43246998+fupn26@users.noreply.github.com>
2018-09-26 17:51:01 +0200

    Add files via upload

---
+++ ch4_drill7.cpp
@@ -0,0 +1,38 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+bool init = true;
+double num;
+string unit;
+double smallest = 0;
+double largest = 0;
+const double m_cm = 100;
+const double in_cm = 2.54;
+const double ft_in = 12;
+const string small = "This is the smallest so far.\n";
+const string large = "This is the largest so far.\n";
+cout<<"Accepted units: cm, m, in, ft\n";
+cout<<"Please enter numbers with one of the units above!\n";
+while (cin>>num>>unit)
+	{
+	cout<<"Your number is: "<<num<<unit<<'\n';
+//	if (init)
+//		{
+//		smallest = num;
+//		largest = num;
+//		init = false;
+//		}
+//	else if (unit == 'm' num < smallest)
+//		{
+//		cout<<small;
+//		smallest = num;
+//		}
+//	else if (largest < num)
+//		{
+//		cout<<large;
+//		largest = num;
+//		} 
+	}
+return 0;
+}
