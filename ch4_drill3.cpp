656bc880e9aa0ab47ee4aaeb3febc2f97e53739e
fupn26 <43246998+fupn26@users.noreply.github.com>
2018-09-26 16:22:53 +0200

    Add files via upload

---
+++ ch4_drill3.cpp
@@ -0,0 +1,21 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int first;
+int second;
+const string small = "the smaller value is: ";
+const string large = "the larger value is: ";
+const string equal = "the numbers are equal";
+cout<<"Please enter two integers!\n";
+while (cin>>first>>second)
+	{
+	if (first == second)
+	cout<<equal<<'\n';
+	else if (first < second)
+	cout<<small<<first<<'\n'<<large<<second<<'\n';
+	else if (first > second)
+	cout<<small<<second<<'\n'<<large<<first<<'\n';
+	return 0;
+	}
+}
